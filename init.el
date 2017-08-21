9c8b119a758899afb34cfbbd6592b0156ebfd65c
laurensmiers <miers132@gmail.com>
2017-08-21 17:20:20 +0200

    Add magit

--- init.el
+++ init.el
@@ -52,6 +52,7 @@
     flycheck-irony
     elpy
     py-autopep8
+	magit
     helm
     helm-gtags
     helm-swoop
@@ -103,7 +104,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (irony-eldoc elpy irony helm-swoop helm))))
+ '(package-selected-packages (quote (magit irony-eldoc elpy irony helm-swoop helm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
