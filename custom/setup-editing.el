5ebf3b0a4b41e4911709677c2aabda118b1e0fc8
laurensmiers <miers132@gmail.com>
2017-08-21 17:28:32 +0200

    Add smartparens config

--- custom/setup-editing.el
+++ custom/setup-editing.el
@@ -51,6 +51,13 @@
 
 ;; Package: smartparens --- smart way to handle (), {}, ...
 (require 'smartparens-config)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
+
+(show-smartparens-global-mode +1)
+(smartparens-global-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keybinding management smartparens ;;
