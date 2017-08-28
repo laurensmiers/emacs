2cba135ce55907e37292054e6844ac12db0434bc
laurensmiers <miers132@gmail.com>
2017-08-28 12:53:00 +0200

    Add initial org-mode settings: - Extra keywords - org root dirs - add note when ending TODO - some initial keymaps

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -1,3 +1,14 @@
-;; TODO
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+
+;; when ending TODO (C-C C-t) end with a note + timestamp
+(setq org-log-done 'note)
+;; Specify root dir to search for agenda files, TODOs, ...
+(setq org-agenda-files '("~/org"))
+;; Add extra states for keywords
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+
 
 (provide 'setup-org)
