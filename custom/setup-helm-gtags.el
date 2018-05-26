ea4b0063d04a9ceb910157c4041c024f8fe18c4f
laurensmiers <miers132@gmail.com>
2018-05-27 00:05:50 +0200

    Add helm-gtags key bindings

--- custom/setup-helm-gtags.el
+++ custom/setup-helm-gtags.el
@@ -7,4 +7,13 @@
 (add-hook 'java-mode-hook 'helm-gtags-mode)
 (add-hook 'asm-mode-hook 'helm-gtags-mode)
 
+;; customize behaviour
+(custom-set-variables
+ '(helm-gtags-auto-update t))
+
+(global-set-key (kbd "M-.") 'helm-gtags-find-tag-from-here)
+(global-set-key (kbd "M-,") 'helm-gtags-pop-stack)
+(global-set-key (kbd "C-c C-f") 'helm-gtags-find-files)
+(global-set-key (kbd "C-c g f") 'helm-gtags-parse-file)
+
 (provide 'setup-helm-gtags)
