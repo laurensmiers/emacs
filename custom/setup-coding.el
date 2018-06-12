96ab07c63805dccf5f6c1829e0fecfa6362e32a2
laurensmiers <miers132@gmail.com>
2018-06-12 22:28:21 +0200

    Add C coding setting + angry faces

--- custom/setup-coding.el
+++ custom/setup-coding.el
@@ -1,35 +1,3 @@
-;; make angry face to get my attention
-(setq prog-modes '(c++-mode python-mode erlang-mode java-mode c-mode emacs-lisp-mode scheme-mode prog-mode))
-(make-face 'font-lock-angry-face)
-(modify-face 'font-lock-angry-face "Red" "Yellow" nil t nil t nil nil)
-
-;; Add keywords to recognize to angry face
-(mapc (lambda (mode)
-		(font-lock-add-keywords
-		 mode
-		 '(("\\<\\(FIXME\\)" 1 'font-lock-angry-face t)))
-		)
-	  prog-modes)
-(mapc (lambda (mode)
-		(font-lock-add-keywords
-		 mode
-		 '(("\\<\\(TODO\\)" 1 'font-lock-angry-face t)))
-		)
-	  prog-modes)
-
-;; default coding style
-(setq c-default-style "linux")
-
-;; sane indentation offset
-(setq c-basic-offset 4)
-
-;; Tab-space strategy
-;; we use spaces, deal with it
-(setq-default indent-tabs-mode nil)
-
-;; Enable subword mode for handling CamelCase format
-(global-subword-mode t)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;              Python                          ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
