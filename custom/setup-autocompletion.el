a79e204d376bcb655c3c1f5aa415524f9a4916e9
laurensmiers <miers132@gmail.com>
2019-08-26 21:57:27 +0200

    Remove old config files

--- custom/setup-autocompletion.el
+++
@@ -1,8 +0,0 @@
-;; Add irony as flycheck hook
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook 'flycheck-irony-setup))
-
-;; Set tab to autocomplete or indent depending on context
-(global-set-key (kbd "<tab>") 'company-indent-or-complete-common)
-
-(provide 'setup-autocompletion)
