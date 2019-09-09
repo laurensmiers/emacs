831031aa3a608eb7aba062984e9b4ce1bfb6f253
laurensmiers <miers132@gmail.com>
2019-09-09 09:27:03 +0200

    Add project-specific config file option

--- init.el
+++ init.el
@@ -17,9 +17,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+;; If it exists, load some project-specific configurations.
+(when (file-readable-p "~/.emacs.d/project.org")
+(org-babel-load-file (expand-file-name "~/.emacs.d/project.org")))
 
 (provide 'init)
 (custom-set-variables
