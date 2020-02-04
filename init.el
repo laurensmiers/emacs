5b39426ab9e3d30cfde8478db67000c902996e0f
laurensmiers <miers132@gmail.com>
2020-02-04 10:49:05 +0100

    INIT: Remove custom-set-variables

--- init.el
+++ init.el
@@ -26,18 +26,3 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/project.org")))
 
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-gtags-auto-update t)
- '(package-selected-packages
-   (quote
-    (elpy linum-relative avy flycheck-irony irony-eldoc company-irony company-c-headers flycheck magit py-autopep8 multiple-cursors helm-gtags helm-swoop zygospore yasnippet volatile-highlights use-package undo-tree smartparens smart-mode-line monokai-theme iedit helm expand-region dashboard comment-dwim-2))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
