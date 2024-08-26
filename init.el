0f9db243ad9f851bdf39f80ca684d0274988c174
Laurens Miers <laurensmiers@protonmail.com>
2024-08-26 16:02:44 +0200

    'Fresh' start
    - Use config_new.org as main config file
      Trimmed down version of old config.
      Insipred by 'mastering emacs' book to use more the built-in
      functionality of emacs. I found that It's more than good enough for my usecases.
    - adapt early-init for quicker startup
      Stolen from doom emacs
    - Don't use/load project.org
      Use .dir-locals.el you peasant

--- init.el
+++ init.el
@@ -5,13 +5,26 @@
 ;;; ...
 (let ((gc-cons-threshold most-positive-fixnum))
   ;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
-  (defvar config-file (expand-file-name "config.org" user-emacs-directory))
-  (defvar project-file (expand-file-name "project.org" user-emacs-directory))
+  (defvar config-file (expand-file-name "config_new.org" user-emacs-directory))
+  ;;(defvar project-file (expand-file-name "project.org" user-emacs-directory))
 
   (when (file-readable-p config-file)
     (org-babel-load-file (expand-file-name config-file)))
 
   ;; If it exists, load some project-specific configurations.
-  (when (file-readable-p project-file)
-    (org-babel-load-file (expand-file-name project-file)))
+  ;;(when (file-readable-p project-file)
+  ;;  (org-babel-load-file (expand-file-name project-file)))
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(flycheck-clang-tidy org-tree-slide ox-reveal writeroom-mode visual-fill-column org-present clang-format+ dash)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
